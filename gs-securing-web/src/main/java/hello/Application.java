08963440a9a3004824c03423003c82477da79f95
jharajesh1 <jha.rajesh1@gmail.com>
2017-02-07 09:47:40 -0600

    Add files via upload

---
+++ gs-securing-web/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by RJha on 2/6/2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
